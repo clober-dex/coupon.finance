69f201116814c93a99dea14ff63090821837f92e
Ion Jo <onemonsterceo@gmail.com>
2023-07-26 11:51:26 +0900

    feat: borrow, deposit mode in url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import Deposit from '../components/deposit'
 import Borrow from '../components/borrow'
 
 const Home: NextPage = () => {
-  // TODO: add to url params
-  const [isDeposit, setIsDeposit] = React.useState(true)
+  const router = useRouter()
 
   return (
     <div className="flex flex-1">
@@ -24,20 +24,24 @@ const Home: NextPage = () => {
         <div className="flex mt-24">
           <button
             className="flex font-bold items-center justify-center text-2xl w-36 bg-transparent text-gray-400 dark:text-gray-500 disabled:text-gray-950 border-0 dark:disabled:text-white rounded-none p-2 border-b-4 border-b-transparent border-t-4 border-t-transparent disabled:border-b-gray-950 dark:disabled:border-b-white"
-            disabled={isDeposit}
-            onClick={() => setIsDeposit(true)}
+            disabled={router.query.mode !== 'borrow'}
+            onClick={() =>
+              router.replace('/?mode=deposit', undefined, { shallow: true })
+            }
           >
             Deposit
           </button>
           <button
             className="flex font-bold items-center justify-center text-2xl w-36 bg-transparent text-gray-400 dark:text-gray-500 disabled:text-gray-950 border-0 dark:disabled:text-white rounded-none p-2 border-b-4 border-b-transparent border-t-4 border-t-transparent disabled:border-b-gray-950 dark:disabled:border-b-white"
-            disabled={!isDeposit}
-            onClick={() => setIsDeposit(false)}
+            disabled={router.query.mode === 'borrow'}
+            onClick={() =>
+              router.replace('/?mode=borrow', undefined, { shallow: true })
+            }
           >
             Borrow
           </button>
         </div>
-        {isDeposit ? <Deposit /> : <Borrow />}
+        {router.query.mode !== 'borrow' ? <Deposit /> : <Borrow />}
       </main>
     </div>
   )
