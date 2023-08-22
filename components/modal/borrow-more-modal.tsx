dce26ff31254c8e7eb54b05caf0da79ae77e77c5
Ion Jo <onemonsterceo@gmail.com>
2023-08-23 02:00:52 +0900

    feat: mobile modal

--- components/modal/borrow-more-modal.tsx
+++ components/modal/borrow-more-modal.tsx
@@ -43,19 +43,19 @@ const BorrowMoreModal = ({
   const [value, setValue] = useState('')
   return (
     <Modal show={!!position} onClose={onClose}>
-      <h1 className="font-bold text-xl mb-6">
+      <h1 className="font-bold text-sm sm:text-xl mb-4 sm:mb-6">
         How much would you like to borrow?
       </h1>
       <div className="mb-4">
         <div className="flex bg-white dark:bg-gray-800 rounded-lg p-3 shadow dark:shadow-none">
           <div className="flex flex-col flex-1 justify-between gap-2">
             <NumberInput
-              className="text-2xl placeholder-gray-400 outline-none bg-transparent"
+              className="text-xl sm:text-2xl placeholder-gray-400 outline-none bg-transparent w-40 sm:w-auto"
               value={value}
               onValueChange={setValue}
               placeholder="0.0000"
             />
-            <div className="text-gray-400 dark:text-gray-500 text-sm">
+            <div className="text-gray-400 dark:text-gray-500 text-xs sm:text-sm">
               ~$0.0000
             </div>
           </div>
@@ -66,9 +66,11 @@ const BorrowMoreModal = ({
                 alt={position?.currency.name}
                 className="w-5 h-5"
               />
-              <div>{position?.currency.symbol}</div>
+              <div className="text-sm sm:text-base">
+                {position?.currency.symbol}
+              </div>
             </div>
-            <div className="flex text-sm gap-2">
+            <div className="flex text-xs sm:text-sm gap-1 sm:gap-2">
               <div className="text-gray-500">Available</div>
               <div>{position?.amount}</div>
               <button className="text-green-500">MAX</button>
@@ -76,9 +78,9 @@ const BorrowMoreModal = ({
           </div>
         </div>
       </div>
-      <div className="flex flex-col mb-8 gap-3 text-sm">
-        <div className="flex gap-3">
-          <div>LTV</div>
+      <div className="flex flex-col mb-6 sm:mb-8 gap-2 sm:gap-3 text-xs sm:text-sm">
+        <div className="flex gap-3 justify-between sm:justify-start">
+          <div className="text-gray-500">LTV</div>
           <div className="flex items-center gap-1">
             <span className="text-green-500">37.28%</span>
             {value ? (
@@ -91,14 +93,14 @@ const BorrowMoreModal = ({
             )}
           </div>
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 justify-between sm:justify-start">
           <div className="text-gray-500">Coupon Purchase Fee</div>
           <div>24.1234 ETH</div>
         </div>
       </div>
       <button
         disabled={true}
-        className="font-bold text-xl disabled:bg-gray-100 dark:disabled:bg-gray-800 h-16 rounded-lg disabled:text-gray-300 dark:disabled:text-gray-500"
+        className="font-bold text-base sm:text-xl disabled:bg-gray-100 dark:disabled:bg-gray-800 h-12 sm:h-16 rounded-lg disabled:text-gray-300 dark:disabled:text-gray-500"
       >
         Confirm
       </button>
