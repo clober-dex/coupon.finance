fd204245cf9cc71ad42680ed54d3cea7042a27f4
graykode <nlkey2022@gmail.com>
2023-08-29 23:21:47 +0900

    feat: add facuet page

---
+++ pages/api/faucet.ts
@@ -0,0 +1,127 @@
+import axios from 'axios'
+import { createWalletClient, http, publicActions } from 'viem'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { privateKeyToAccount } from 'viem/accounts'
+
+import { couponFinanceChain } from '../../utils/dev-chain'
+
+const FAUCET_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'address[]',
+        name: 'tokens',
+        type: 'address[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: 'amounts',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+    ],
+    name: 'faucetTokens',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+] as const
+
+const verifyRecaptcha = async (token: string) => {
+  const secretKey = process.env.RECAPTHA_SECRET_KEY
+
+  const verificationUrl =
+    'https://www.google.com/recaptcha/api/siteverify?secret=' +
+    secretKey +
+    '&response=' +
+    token
+
+  const result = await axios.post(verificationUrl)
+  return result
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  try {
+    const token = req.body.gRecaptchaToken
+
+    // Recaptcha response
+    const response = await verifyRecaptcha(token)
+
+    // Checking if the reponse sent by reCaptcha success or not and if the score is above 0.5
+    // In ReCaptcha v3, a score sent which tells if the data sent from front end is from Human or from Bots. If score above 0.5 then it is human otherwise it is bot
+    // For more info check, https://developers.google.com/recaptcha/docs/v3
+    // ReCaptcha v3 response, {
+    //     "success": true|false,      // whether this request was a valid reCAPTCHA token for your site
+    //     "score": number             // the score for this request (0.0 - 1.0)
+    //     "action": string            // the action name for this request (important to verify)
+    //     "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+    //     "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+    //     "error-codes": [...]        // optional
+    //   }
+    if (response.data.success && response.data.score >= 0.5) {
+      //INSERT API/LOGIC for saving data once the validation is complete
+      const userAddress = req.body.address
+      if (!userAddress) {
+        return res.json({
+          status: 'error',
+          message: 'Something went wrong, please try again!!!',
+        })
+      }
+      const account = privateKeyToAccount(
+        process.env.DEV_PRIVATE_KEY as `0x${string}`,
+      )
+      const client = createWalletClient({
+        account,
+        chain: couponFinanceChain,
+        transport: http(),
+      }).extend(publicActions)
+      const { request } = await client.simulateContract({
+        address: '0xE1B33C4099C37A48d70599BB39a48870c7eaDC81', // Faucet address
+        abi: FAUCET_ABI,
+        functionName: 'faucetTokens',
+        args: [
+          [
+            '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // WETH
+            '0xaf88d065e77c8cC2239327C5EDb3A432268e5831', // USDC
+            '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1', // DAI
+            '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9', // USDT
+            '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f', // WBTC
+          ],
+          [
+            100000000000000000n, // 0.1 WETH
+            100000000n, // 100 USDC
+            100000000000000000000n, // 100 DAI
+            100000000n, // 100 USDT
+            10000000n, // 0.1 WBTC
+          ],
+          userAddress,
+        ],
+        account: account.address,
+      })
+      const hash = await client.writeContract(request)
+
+      return res.json({
+        status: 'success',
+        txHash: hash,
+      })
+    } else {
+      return res.json({
+        status: 'error',
+        message: 'Something went wrong, please try again!!!',
+      })
+    }
+  } catch (error) {
+    console.log('ERRRRROr', error)
+    res.json({
+      status: 'error',
+      message: 'Something went wrong, please try again!!!',
+    })
+  }
+}
---
+++ pages/faucet/index.tsx
@@ -0,0 +1,165 @@
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
+import Head from 'next/head'
+import React, { useCallback, useState } from 'react'
+import {
+  GoogleReCaptchaProvider,
+  useGoogleReCaptcha,
+} from 'react-google-recaptcha-v3'
+import { getAddress } from 'viem'
+
+import { useTransactionContext } from '../../contexts/transaction-context'
+import { Asset } from '../../model/asset'
+import { fetchAssets } from '../../api/asset'
+
+export const FAUCET_AMOUNTS: { [symbol: string]: number } = {
+  ETH: 0.1,
+  USDC: 100,
+  DAI: 100,
+  USDT: 100,
+  WBTC: 0.1,
+}
+
+const FaucetForm = ({ assets }: { assets: Asset[] }) => {
+  const { executeRecaptcha } = useGoogleReCaptcha()
+  const { setConfirmation } = useTransactionContext()
+  const [address, setAddress] = useState('')
+  const submitEnquiryForm = async (gReCaptchaToken: any) => {
+    setConfirmation({
+      title: 'Faucet',
+      body: 'You will receive test tokens in a few seconds.',
+      fields: assets.map((asset) => ({
+        currency: asset.underlying,
+        label: asset.underlying.symbol,
+        value: FAUCET_AMOUNTS[asset.underlying.symbol].toString(),
+      })),
+    })
+
+    const response = await fetch('/api/faucet', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        address,
+        gRecaptchaToken: gReCaptchaToken,
+      }),
+    })
+
+    if (response.status === 200) {
+      const { txHash } = await response.json()
+      window.open(`http://dev-rpc.coupon.finance:4000/tx/${txHash}`, '_blank')
+      setConfirmation(undefined)
+    }
+  }
+
+  const handleSumitForm = useCallback(
+    (e: any) => {
+      e.preventDefault()
+      if (!executeRecaptcha) {
+        return
+      }
+      executeRecaptcha('enquiryFormSubmit').then((gReCaptchaToken) => {
+        submitEnquiryForm(gReCaptchaToken)
+      })
+    },
+    [executeRecaptcha, submitEnquiryForm],
+  )
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const address = event.target.value.match(
+        /^0x[a-fA-F0-9]{40}$|^0x[a-fA-F0-9]{42}$/,
+      )
+      if (address && address[0]) {
+        setAddress(getAddress(address[0]))
+      }
+    },
+    [setAddress],
+  )
+  return (
+    <form
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '4px',
+      }}
+      className="faucet-form"
+      method="POST"
+      onSubmit={handleSumitForm}
+    >
+      <div className="flex flex-col gap-4">
+        <div className="font-bold text-sm sm:text-lg">
+          Write your address to get test tokens
+        </div>
+        <div className="flex flex-col bg-white dark:bg-gray-800 rounded-lg p-3 gap-2">
+          <div className="flex flex-1 justify-between gap-2">
+            <input
+              className="flex-1 text-xl sm:text-2xl placeholder-gray-400 outline-none bg-transparent"
+              value={address}
+              name="address"
+              type="text"
+              onChange={handleChange}
+              placeholder="0x0000000000000000000000000000000000000000"
+              required={true}
+            />
+          </div>
+        </div>
+      </div>
+      <button
+        type="submit"
+        disabled={address.length === 0}
+        className="font-bold text-base sm:text-xl bg-green-500 disabled:bg-gray-100 dark:disabled:bg-gray-800 h-12 sm:h-16 rounded-lg text-white disabled:text-gray-300 dark:disabled:text-gray-500"
+      >
+        Faucet
+      </button>
+    </form>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  assets: Asset[]
+}> = async () => {
+  const assets = await fetchAssets()
+  return {
+    props: { assets },
+  }
+}
+
+const Faucet: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ assets }) => {
+  return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey="6Lce3uMnAAAAALuDAg71x3qku80Avq7xl_y8O2OH"
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: 'head',
+        nonce: undefined,
+      }}
+    >
+      <div className="flex flex-1">
+        <Head>
+          <title>Coupon Finance</title>
+          <meta
+            content="Cash in the coupons on your assets. The only liquidity protocol that enables a 100% utilization rate."
+            name="description"
+          />
+          <link href="/favicon.svg" rel="icon" />
+        </Head>
+
+        <main className="flex flex-1 flex-col justify-center items-center">
+          <div className="flex flex-1 flex-col w-full">
+            <div className="flex flex-1 sm:items-center justify-center">
+              <div className="flex flex-col sm:shadow bg-gray-50 dark:bg-gray-900 sm:rounded-3xl p-4 sm:p-6 w-full sm:w-[480px] gap-8">
+                <FaucetForm assets={assets} />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </GoogleReCaptchaProvider>
+  )
+}
+
+export default Faucet
