6b880af4c144f336b26e52557a64744d7f41b740
graykode <nlkey2022@gmail.com>
2023-08-30 20:35:36 +0900

    chore: remove console.log

--- api/epoch.ts
+++ api/epoch.ts
@@ -2,11 +2,11 @@ import { Epoch } from '../model/epoch'
 import { getBuiltGraphSDK } from '../.graphclient'
 
 const { getEpochs } = getBuiltGraphSDK()
+
 export async function fetchEpochs(): Promise<Epoch[]> {
   const { epoches } = await getEpochs({
     timestamp: Math.floor(new Date().getTime() / 1000).toString(),
   })
-  console.log('epoches', epoches)
   return epoches.map((epoch) => ({
     id: BigInt(epoch.id),
     startTimestamp: BigInt(epoch.startTimestamp),
