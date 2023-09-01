589850289a69f9bf44531c17221830bd45a0490e
graykode <nlkey2022@gmail.com>
2023-09-01 12:09:31 +0900

    feat: support arbitrum goeril for testnet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import {
 } from '@rainbow-me/rainbowkit'
 import type { AppProps } from 'next/app'
 import { configureChains, createConfig, useAccount, WagmiConfig } from 'wagmi'
-import { arbitrum } from 'wagmi/chains'
+import { arbitrumGoerli } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import {
@@ -29,7 +29,7 @@ import { CurrencyProvider } from '../contexts/currency-context'
 import { TransactionProvider } from '../contexts/transaction-context'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [process.env.BUILD === 'dev' ? couponFinanceChain : arbitrum],
+  [process.env.BUILD === 'dev' ? couponFinanceChain : arbitrumGoerli],
   [
     alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || '' }),
     publicProvider(),
