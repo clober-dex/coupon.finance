d77dac59ab6fc0f1cc0b9384d8489051ece1c9a6
graykode <nlkey2022@gmail.com>
2023-09-06 00:18:40 +0900

    refactor: `selected` -> `epochs`

--- components/modal/edit-expiry-modal.tsx
+++ components/modal/edit-expiry-modal.tsx
@@ -18,7 +18,7 @@ const EditExpiryModal = ({
 }) => {
   const { balances } = useCurrencyContext()
   const { extendLoanDuration, shortenLoanDuration } = useBorrowContext()
-  const [selected, setSelected] = useState(0)
+  const [epochs, setEpochs] = useState(0)
 
   const { data } = useQuery(['coupon-amount-to-edit-expiry', position], () =>
     fetchCouponAmountByEpochsBorrowed(
@@ -41,23 +41,23 @@ const EditExpiryModal = ({
     }
     return [
       data
-        .slice(expiryEpochIndex, selected)
+        .slice(expiryEpochIndex, epochs)
         .reduce((acc, { interest }) => acc + interest, 0n),
       data
-        .slice(expiryEpochIndex, selected)
+        .slice(expiryEpochIndex, epochs)
         .reduce((acc, { payable }) => acc && payable, true),
       data
-        .slice(selected - 1, expiryEpochIndex - 1)
+        .slice(epochs - 1, expiryEpochIndex - 1)
         .reduce((acc, { refund }) => acc + refund, 0n),
       data
-        .slice(selected - 1, expiryEpochIndex - 1)
+        .slice(epochs - 1, expiryEpochIndex - 1)
         .reduce((acc, { refundable }) => acc && refundable, true),
     ]
-  }, [data, expiryEpochIndex, selected])
+  }, [data, expiryEpochIndex, epochs])
 
   useEffect(() => {
     if (expiryEpochIndex > 0) {
-      setSelected(expiryEpochIndex)
+      setEpochs(expiryEpochIndex)
     }
   }, [expiryEpochIndex, position])
 
@@ -73,8 +73,8 @@ const EditExpiryModal = ({
         <div className="px-6 mb-2">
           <Slider
             length={data?.length ?? 0}
-            value={selected}
-            onValueChange={setSelected}
+            value={epochs}
+            onValueChange={setEpochs}
           />
         </div>
         <div className="flex justify-between">
@@ -82,7 +82,7 @@ const EditExpiryModal = ({
             <button
               key={i}
               className="flex flex-col items-center gap-2 w-[72px]"
-              onClick={() => setSelected(i + 1)}
+              onClick={() => setEpochs(i + 1)}
             >
               <div className="text-sm">{date}</div>
             </button>
@@ -91,8 +91,8 @@ const EditExpiryModal = ({
       </div>
       <button
         disabled={
-          selected === 0 ||
-          expiryEpochIndex === selected ||
+          epochs === 0 ||
+          expiryEpochIndex === epochs ||
           (refund === 0n && interest === 0n) ||
           interest > balances[position.underlying.address] ||
           !payable ||
@@ -100,32 +100,32 @@ const EditExpiryModal = ({
         }
         className="font-bold text-base sm:text-xl bg-green-500 disabled:bg-gray-100 dark:disabled:bg-gray-800 h-12 sm:h-16 rounded-lg text-white disabled:text-gray-300 dark:disabled:text-gray-500"
         onClick={async () => {
-          if (selected > expiryEpochIndex) {
+          if (epochs > expiryEpochIndex) {
             await extendLoanDuration(
               position.underlying,
               position.positionId,
-              selected - expiryEpochIndex,
+              epochs - expiryEpochIndex,
               interest,
             )
-          } else if (selected < expiryEpochIndex) {
+          } else if (epochs < expiryEpochIndex) {
             await shortenLoanDuration(
               position.underlying,
               position.positionId,
-              expiryEpochIndex - selected,
+              expiryEpochIndex - epochs,
               refund,
             )
           }
-          setSelected(0)
+          setEpochs(0)
           onClose()
         }}
       >
-        {selected === 0
+        {epochs === 0
           ? 'Select expiry date'
-          : expiryEpochIndex === selected
+          : expiryEpochIndex === epochs
           ? 'Select expiry date'
-          : selected > expiryEpochIndex && !payable
+          : epochs > expiryEpochIndex && !payable
           ? 'Not enough coupons for pay'
-          : selected < expiryEpochIndex && !refundable
+          : epochs < expiryEpochIndex && !refundable
           ? 'Not enough coupons for refund'
           : interest > balances[position.underlying.address]
           ? `Insufficient ${position.underlying.symbol} balance`
