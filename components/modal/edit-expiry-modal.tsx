49e63f4bc4bfc3e4e0d2345aa80ba643d1bd19f6
Ion Jo <onemonsterceo@gmail.com>
2023-09-05 22:01:35 +0900

    feat: click to unselect epoch

--- components/modal/edit-expiry-modal.tsx
+++ components/modal/edit-expiry-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useQuery } from 'wagmi'
 
 import Slider from '../slider'
@@ -18,7 +18,14 @@ const EditExpiryModal = ({
 }) => {
   const { balances } = useCurrencyContext()
   const { extendLoanDuration, shortenLoanDuration } = useBorrowContext()
-  const [epochs, setEpochs] = useState(0)
+  const [epochs, _setEpochs] = useState(0)
+
+  const setEpochs = useCallback(
+    (value: number) => {
+      _setEpochs(value === epochs ? value - 1 : value)
+    },
+    [epochs],
+  )
 
   const { data } = useQuery(['coupon-amount-to-edit-expiry', position], () =>
     fetchCouponAmountByEpochsBorrowed(
@@ -42,9 +49,9 @@ const EditExpiryModal = ({
 
   useEffect(() => {
     if (expiryEpochIndex > 0) {
-      setEpochs(expiryEpochIndex)
+      _setEpochs(expiryEpochIndex)
     }
-  }, [expiryEpochIndex, position])
+  }, [expiryEpochIndex, position, _setEpochs])
 
   return (
     <Modal show onClose={onClose}>
@@ -106,7 +113,7 @@ const EditExpiryModal = ({
         {epochs === 0
           ? 'Select expiry date'
           : expiryEpochIndex === epochs
-          ? 'Select expiry date'
+          ? 'Select new expiry date'
           : (refund === 0n && interest === 0n) || interest > available
           ? 'Not enough coupons for sale'
           : interest > balances[position.underlying.address]
