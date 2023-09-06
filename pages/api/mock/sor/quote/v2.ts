af56b785b6aaff2d908fef3a7ed28f65c3a692ef
graykode <nlkey2022@gmail.com>
2023-09-06 21:44:28 +0900

    feat: return `pathId` from mocking odos api

--- pages/api/mock/sor/quote/v2.ts
+++ pages/api/mock/sor/quote/v2.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getAddress } from 'viem'
 import { readContracts } from '@wagmi/core'
+import { ethers } from 'ethers'
 
 import { CONTRACT_ADDRESSES } from '../../../../../utils/addresses'
 import { CouponOracle__factory, ERC20__factory } from '../../../../../typechain'
@@ -61,11 +62,21 @@ export default async function handler(
     (BigInt(prices?.[1] ?? 1n) *
       10n ** (18n - BigInt(outputTokenDecimals ?? 18n)))
 
+  const iface = new ethers.utils.Interface([
+    'function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut) external',
+  ])
+  const pathId = iface.encodeFunctionData('swap', [
+    inputTokens[0].tokenAddress,
+    outputTokens[0].tokenAddress,
+    inputTokens[0].amount,
+    outputAmount.toString(),
+  ])
+
   return res.json({
     inTokens: [inputTokens[0].tokenAddress],
     outTokens: [outputTokens[0].tokenAddress],
     inAmounts: [inputTokens[0].amount],
     outAmounts: [outputAmount.toString()],
-    pathId: '798a17c3a7419b3f260ea3447198d02f',
+    pathId,
   })
 }
