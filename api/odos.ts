729208756a6c3af1f55ab79633b1ed9c53b96a85
graykode <nlkey2022@gmail.com>
2023-09-07 01:35:16 +0900

    refactor: move `fetchOdosApi` to api/odos

--- api/odos.ts
+++ api/odos.ts
@@ -1,4 +1,20 @@
-import { fetchOdosApi } from '../utils/odos'
+export async function fetchOdosApi<T>(
+  path: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_ODOS_API_BASE_URL}/${path}`,
+    options,
+  )
+
+  if (response.ok) {
+    return response.json()
+  } else {
+    const errorResponse = await response.json()
+
+    throw new Error(errorResponse.message || 'Unknown Error')
+  }
+}
 
 export async function fetchCallDataByOdos({
   pathId,
