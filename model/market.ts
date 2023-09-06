3c2c08253de05253f0eb9656f7caf19884f6e7f3
graykode <nlkey2022@gmail.com>
2023-09-06 12:58:52 +0900

    feat: remove multiply small percentage

--- model/market.ts
+++ model/market.ts
@@ -460,7 +460,7 @@ export function calculateCouponsToWithdraw(
   return {
     maxRepurchaseFee,
     repurchaseFee,
-    available: BigInt(Math.floor(Number(available) * (1 - 0.0001))), // 0.01%
+    available,
   }
 }
 
@@ -518,6 +518,6 @@ export function calculateCouponsToBorrow(
   return {
     interest,
     maxInterest,
-    available: BigInt(Math.floor(Number(available) * (1 - 0.0001))), // 0.01%
+    available,
   }
 }
