0119fef77e64a79aa41a7acb355f7a2d1ca34447
graykode <nlkey2022@gmail.com>
2023-09-09 23:18:25 +0900

    feat: create AccountProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import '../styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import {
@@ -8,13 +8,21 @@ import {
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit'
 import type { AppProps } from 'next/app'
-import { configureChains, createConfig, useAccount, WagmiConfig } from 'wagmi'
+import { SessionProvider, signIn } from 'next-auth/react'
+import {
+  configureChains,
+  ConnectorData,
+  createConfig,
+  useAccount,
+  WagmiConfig,
+} from 'wagmi'
 import { arbitrumGoerli } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { identify } from '@web3analytic/funnel-sdk'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import Header from '../components/header'
 import { ThemeProvider, useThemeContext } from '../contexts/theme-context'
@@ -82,6 +90,44 @@ const Web3AnalyticWrapper = ({ children }: React.PropsWithChildren) => {
   return <>{children}</>
 }
 
+const AccountProvider = ({ children }: React.PropsWithChildren) => {
+  const router = useRouter()
+  const callbackUrl = (router.query.callbackUrl as string) ?? '/'
+  const login = useCallback(
+    async ({ address }: { address: `0x${string}` }) => {
+      const response = await signIn('credentials', {
+        address,
+        redirect: false,
+      })
+      if (!response?.error) {
+        await router.push(callbackUrl)
+      }
+    },
+    [callbackUrl, router],
+  )
+
+  const { connector: activeConnector } = useAccount({
+    onConnect({ address }) {
+      if (address) {
+        login({ address })
+      }
+    },
+  })
+
+  useEffect(() => {
+    const handleConnectorUpdate = ({ account }: ConnectorData) => {
+      if (account) {
+        login({ address: account })
+      }
+    }
+    if (activeConnector) {
+      activeConnector.on('change', handleConnectorUpdate)
+    }
+    return () => activeConnector?.off('change', handleConnectorUpdate) as any
+  }, [activeConnector, login])
+  return <>{children}</>
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState(false)
   return (
@@ -94,34 +140,38 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="/favicon.svg" rel="icon" />
       </Head>
       <ThemeProvider>
-        <WalletProvider>
-          <Web3AnalyticWrapper>
-            <TransactionProvider>
-              <CurrencyProvider>
-                <DepositProvider>
-                  <BorrowProvider>
-                    <div className="flex flex-col w-screen min-h-screen bg-gray-50 dark:bg-gray-950 text-gray-950 dark:text-white">
-                      <Panel open={open} setOpen={setOpen} />
-                      <Header onMenuClick={() => setOpen(true)} />
-                      <Component {...pageProps} />
-                      <Link
-                        target="_blank"
-                        href="https://github.com/clober-dex/coupon.finance"
-                        className="fixed right-4 bottom-4 bg-gray-200 dark:bg-gray-800 rounded-full text-xs px-4 py-1"
-                      >
-                        #
-                        {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(
-                          0,
-                          7,
-                        )}
-                      </Link>
-                    </div>
-                  </BorrowProvider>
-                </DepositProvider>
-              </CurrencyProvider>
-            </TransactionProvider>
-          </Web3AnalyticWrapper>
-        </WalletProvider>
+        <SessionProvider refetchInterval={0} session={pageProps.session}>
+          <WalletProvider>
+            <AccountProvider>
+              <Web3AnalyticWrapper>
+                <TransactionProvider>
+                  <CurrencyProvider>
+                    <DepositProvider>
+                      <BorrowProvider>
+                        <div className="flex flex-col w-screen min-h-screen bg-gray-50 dark:bg-gray-950 text-gray-950 dark:text-white">
+                          <Panel open={open} setOpen={setOpen} />
+                          <Header onMenuClick={() => setOpen(true)} />
+                          <Component {...pageProps} />
+                          <Link
+                            target="_blank"
+                            href="https://github.com/clober-dex/coupon.finance"
+                            className="fixed right-4 bottom-4 bg-gray-200 dark:bg-gray-800 rounded-full text-xs px-4 py-1"
+                          >
+                            #
+                            {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(
+                              0,
+                              7,
+                            )}
+                          </Link>
+                        </div>
+                      </BorrowProvider>
+                    </DepositProvider>
+                  </CurrencyProvider>
+                </TransactionProvider>
+              </Web3AnalyticWrapper>
+            </AccountProvider>
+          </WalletProvider>
+        </SessionProvider>
       </ThemeProvider>
     </>
   )
