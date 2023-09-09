8752a8617a19ae1718cee63bb78f46cb2d67c67a
graykode <nlkey2022@gmail.com>
2023-09-10 01:04:40 +0900

    feat: ssr for positions

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query'
+import { getToken } from 'next-auth/jwt'
+import { useAccount } from 'wagmi'
 
 import Deposit from '../components/deposit'
 import Borrow from '../components/borrow'
@@ -11,11 +14,26 @@ import { AssetStatus } from '../model/asset'
 import { fetchAssetStatuses } from '../api/asset'
 import { fetchEpochs } from '../api/epoch'
 import { Epoch } from '../model/epoch'
+import { fetchBondPositions } from '../api/bond-position'
+import { fetchLoanPositions } from '../api/loan-position'
 
 export const getServerSideProps: GetServerSideProps<{
   assetStatuses: AssetStatus[]
   epochs: Epoch[]
-}> = async () => {
+}> = async ({ req }) => {
+  const queryClient = new QueryClient()
+  const token = await getToken({ req })
+  const userAddress = token?.sub as `0x${string}` | undefined
+
+  await queryClient.prefetchQuery(
+    ['bond-positions', userAddress],
+    () => userAddress && fetchBondPositions(userAddress),
+  )
+  await queryClient.prefetchQuery(
+    ['loan-positions', userAddress],
+    () => userAddress && fetchLoanPositions(userAddress),
+  )
+
   const [assetStatuses, epochs] = await Promise.all([
     fetchAssetStatuses(),
     fetchEpochs(),
@@ -24,6 +42,7 @@ export const getServerSideProps: GetServerSideProps<{
     props: {
       assetStatuses,
       epochs,
+      dehydratedState: dehydrate(queryClient),
     },
   }
 }
@@ -32,6 +51,27 @@ const Home: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ assetStatuses, epochs }) => {
   const router = useRouter()
+  const { address: userAddress } = useAccount()
+
+  const { data: bondPositions } = useQuery(
+    ['bond-positions', userAddress],
+    () => (userAddress ? fetchBondPositions(userAddress) : []),
+    {
+      refetchOnWindowFocus: true,
+      refetchInterval: 2 * 1000,
+      initialData: [],
+    },
+  )
+
+  const { data: loanPositions } = useQuery(
+    ['loan-positions', userAddress],
+    () => (userAddress ? fetchLoanPositions(userAddress) : []),
+    {
+      refetchOnWindowFocus: true,
+      refetchInterval: 2 * 1000,
+      initialData: [],
+    },
+  )
 
   return (
     <div className="flex flex-1">
@@ -67,9 +107,17 @@ const Home: NextPage<
           </Link>
         </div>
         {router.query.mode !== 'borrow' ? (
-          <Deposit assetStatuses={assetStatuses} epochs={epochs} />
+          <Deposit
+            assetStatuses={assetStatuses}
+            epochs={epochs}
+            positions={bondPositions}
+          />
         ) : (
-          <Borrow assetStatuses={assetStatuses} epochs={epochs} />
+          <Borrow
+            assetStatuses={assetStatuses}
+            epochs={epochs}
+            positions={loanPositions}
+          />
         )}
       </main>
     </div>
