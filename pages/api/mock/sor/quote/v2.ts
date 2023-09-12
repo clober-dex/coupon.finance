2d1e8629b132e9012567bc5de7bc469e874891d4
graykode <nlkey2022@gmail.com>
2023-09-12 15:30:26 +0900

    fix: use only viem for prod env

--- pages/api/mock/sor/quote/v2.ts
+++ pages/api/mock/sor/quote/v2.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getAddress } from 'viem'
-import { readContracts } from '@wagmi/core'
+import { createPublicClient, getAddress, http } from 'viem'
 import { ethers } from 'ethers'
+import { arbitrum, arbitrumGoerli } from 'viem/chains'
 
 import { CONTRACT_ADDRESSES } from '../../../../../utils/addresses'
 import { CouponOracle__factory, ERC20__factory } from '../../../../../typechain'
+import { couponFinanceChain } from '../../../../../utils/dev-chain'
 
 export default async function handler(
   req: NextApiRequest,
@@ -27,41 +28,55 @@ export default async function handler(
       }[]
     } = req.body
 
+    const publicClient = createPublicClient({
+      chain: {
+        [42161]: arbitrum,
+        [421613]: arbitrumGoerli,
+        [7777]: couponFinanceChain,
+      }[chainId],
+      transport: http(),
+    })
+
     const [
       { result: prices },
       { result: inputTokenDecimals },
       { result: outputTokenDecimals },
-    ] = await readContracts({
+    ] = await publicClient.multicall({
       contracts: [
         {
           address: CONTRACT_ADDRESSES.CouponOracle,
           abi: CouponOracle__factory.abi,
           functionName: 'getAssetsPrices',
           args: [[inputTokens[0].tokenAddress, outputTokens[0].tokenAddress]],
-          chainId,
         },
         {
           address: getAddress(inputTokens[0].tokenAddress),
           abi: ERC20__factory.abi,
           functionName: 'decimals',
-          chainId,
         },
         {
           address: getAddress(outputTokens[0].tokenAddress),
           abi: ERC20__factory.abi,
           functionName: 'decimals',
-          chainId,
         },
       ],
     })
+    if (!prices || !inputTokenDecimals || !outputTokenDecimals) {
+      return res.json({
+        inTokens: [],
+        outTokens: [],
+        inAmounts: ['0'],
+        outAmounts: ['0'],
+        pathId: '',
+      })
+    }
 
     const inputAmount = BigInt(inputTokens[0].amount)
     const outputAmount =
       (inputAmount *
-        BigInt(prices?.[0] ?? 1n) *
+        BigInt(prices[0]) *
         10n ** (18n - BigInt(inputTokenDecimals ?? 18n))) /
-      (BigInt(prices?.[1] ?? 1n) *
-        10n ** (18n - BigInt(outputTokenDecimals ?? 18n)))
+      (BigInt(prices[1]) * 10n ** (18n - BigInt(outputTokenDecimals ?? 18n)))
 
     const iface = new ethers.utils.Interface([
       'function swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut) external',
