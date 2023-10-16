d05ba9fb00bd53a80786448a3592c92dde45dbcc
graykode <nlkey2022@gmail.com>
2023-10-16 12:55:25 +0900

    feat: init storybook

---
+++ components/slippage-select.stories.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+
+import '../styles/globals.css'
+import SlippageSelect from './slippage-select'
+
+export default {
+  title: 'Common/SlippageSelect',
+  component: SlippageSelect,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta<typeof SlippageSelect>
+
+const SlippageSelecWithHooks = () => {
+  const [showSlippageSelect, setShowSlippageSelect] = useState(false)
+  const [slippage, setSlippage] = useState('1')
+
+  return (
+    <SlippageSelect
+      show={showSlippageSelect}
+      setShow={setShowSlippageSelect}
+      slippage={slippage}
+      setSlippage={setSlippage}
+    />
+  )
+}
+
+export const Example: StoryObj<typeof SlippageSelect> = {
+  render: () => <SlippageSelecWithHooks />,
+}
