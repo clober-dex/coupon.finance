89fe816f52c8bbdf56dd463833c44f656ba41116
Xavier <ladig1017@gmail.com>
2023-10-16 15:34:46 +0900

    feat: add borrow more modal story book

---
+++ components/modal/borrow-more-modal.stories.tsx
@@ -0,0 +1,89 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import '../../styles/globals.css'
+import BigNumber from 'bignumber.js'
+
+import BorrowMoreModal from './borrow-more-modal'
+
+export default {
+  title: 'BorrowMoreModal',
+  component: BorrowMoreModal,
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof BorrowMoreModal>
+
+type Story = StoryObj<typeof BorrowMoreModal>
+
+export const Default: Story = {
+  args: {
+    position: {
+      id: 0n,
+      substitute: {
+        address: '0x000000',
+        name: 'ETH',
+        symbol: 'ETH',
+        decimals: 18,
+      },
+      underlying: {
+        address: '0x000000',
+        name: 'ETH',
+        symbol: 'ETH',
+        decimals: 18,
+      },
+      collateral: {
+        underlying: {
+          address: '0x000000',
+          name: 'ETH',
+          symbol: 'ETH',
+          decimals: 18,
+        },
+        substitute: {
+          address: '0x000000',
+          name: 'ETH',
+          symbol: 'ETH',
+          decimals: 18,
+        },
+        liquidationThreshold: '0',
+        liquidationTargetLtv: '0',
+      },
+      interest: 10n,
+      amount: 110n,
+      collateralAmount: 10n,
+      fromEpoch: {
+        id: 0,
+        startTimestamp: 0,
+        endTimestamp: 0,
+      },
+      toEpoch: {
+        id: 0,
+        startTimestamp: 0,
+        endTimestamp: 0,
+      },
+      createdAt: 0,
+    },
+    onClose: () => {},
+    value: '',
+    setValue: () => {},
+    prices: {
+      '0x000000': {
+        value: 0n,
+        decimals: 18,
+      },
+    },
+    maxLoanAmount: 0n,
+    currentLtv: BigNumber(0),
+    expectedLtv: 0,
+    interest: 0n,
+    amount: 0n,
+    available: 0n,
+    maxInterest: 0n,
+    maxLoanAmountExcludingCouponFee: 0n,
+    borrowMore: async () => {},
+  },
+}
+
+// @ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString()
+}
