b3e4d4972ea9a5ba617670c0961f5e3e8af946dd
graykode <nlkey2022@gmail.com>
2023-10-24 13:51:52 +0900

    feat: create action button dumb component

---
+++ components/action-button.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const ActionButton = ({
+  disabled,
+  onClick,
+  text,
+  ...props
+}: {
+  disabled: boolean
+  onClick: () => void
+  text: string
+} & React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className="w-full font-bold text-base sm:text-xl bg-green-500 disabled:bg-gray-100 dark:disabled:bg-gray-800 h-12 sm:h-16 rounded-lg text-white disabled:text-gray-300 dark:disabled:text-gray-500"
+      {...props}
+    >
+      {text}
+    </button>
+  )
+}
