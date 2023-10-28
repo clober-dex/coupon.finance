7af17839fedddb3ef21e485f47f6232e915bc286
graykode <nlkey2022@gmail.com>
2023-10-28 13:26:19 +0900

    feat: remove formatDate in epoch file

--- utils/epoch.ts
+++ utils/epoch.ts
@@ -6,6 +6,3 @@ export const getEpoch = (timestamp: number): bigint => {
   const half = new Date(`${year}-07-01`).getTime()
   return BigInt((year - 1970) * 2 + (date.getTime() < half ? 0 : 1))
 }
-
-export const formatDate = (epoch: Epoch): string =>
-  new Date(Number(epoch.endTimestamp) * 1000).toISOString().slice(0, 10)
