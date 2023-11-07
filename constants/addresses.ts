f3d0a968a7ab7920451a86a48fbf5a962c2edaa6
graykode <nlkey2022@gmail.com>
2023-11-07 16:04:57 +0900

    feat: update contract addresses

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -25,15 +25,15 @@ export const CONTRACT_ADDRESSES: {
   },
   [CHAIN_IDS.COUPON_FINANCE_CHAIN]: {
     BondPositionManager:
-      '0xA0D476c6A39beA239749C566a02343e5584Ec200' as `0x${string}`,
+      '0x98C0a86096b508Fae31Ba2aF94E4EDcc29D8001E' as `0x${string}`,
     BorrowController:
-      '0x929075bdc8cf2e43cA7FB4BF1a189130b6014Cc1' as `0x${string}`,
-    CouponOracle: '0xb523dFFc24220A18eB035A50b17Da69E4daea480' as `0x${string}`,
+      '0xC14f853a38444D94f74e05156881BDC154A54D31' as `0x${string}`,
+    CouponOracle: '0xbaF0FF012884917b1FCb2222d6e6D75Eb795D23B' as `0x${string}`,
     DepositController:
-      '0x92bb5C37868C5B34B163FeFAb4e20b1179853eB9' as `0x${string}`,
+      '0x64501F8ad7ff7263c17369EBf287E72d0B571567' as `0x${string}`,
     LoanPositionManager:
-      '0x8831c769874fF23ED5DF0daacfD84Cc147335506' as `0x${string}`,
+      '0xa87224d1F96cA183CE119f94b6e48035c93B05Fb' as `0x${string}`,
     OdosRepayAdapter:
-      '0x00644a534bDea310ee2FCCF1c2821Df769A0b12F' as `0x${string}`,
+      '0x9171E3607dB356975b5d39792657De48a4758B4A' as `0x${string}`,
   },
 }
