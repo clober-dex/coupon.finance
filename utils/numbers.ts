f5ebd7d588b6b3e76f1212c0b1f87fabc336793a
graykode <nlkey2022@gmail.com>
2023-11-08 19:55:29 +0900

    fix: remove `toPlacesString`

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -35,35 +35,11 @@ export const formatUnits = (
 ): string => {
   const formatted = _formatUnits(value, decimals)
   if (!price) {
-    return toPlacesString(formatted)
+    return formatted
   }
   const priceValue = Number(price.value) / 10 ** price.decimals
   const underHalfPennyDecimals =
     Math.floor(Math.max(-Math.log10(0.005 / priceValue), 0) / 2) * 2
   const fixed = new BigNumber(formatted).toFixed(underHalfPennyDecimals)
   return +fixed === 0 ? formatted : fixed
-}
-
-export const getDecimalPlaces = (
-  number: BigNumber.Value,
-  places: number = 4,
-) => {
-  const TEN = new BigNumber(10)
-  const value = new BigNumber(number)
-  if (value.eq(0)) {
-    return places
-  }
-  if (value.gte(TEN.pow(places))) {
-    return 0
-  }
-  let i = 0
-  while (value.abs().lt(TEN.pow(-i * places))) {
-    i += 1
-  }
-  return i ? i * places : 4
-}
-
-export const toPlacesString = (number: BigNumber.Value, places: number = 4) => {
-  const value = new BigNumber(number)
-  return value.toFixed(getDecimalPlaces(number, places))
 }
