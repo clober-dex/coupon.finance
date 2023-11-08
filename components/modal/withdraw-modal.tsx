a329f368115222271d7176ba1232e01f853bafeb
graykode <nlkey2022@gmail.com>
2023-11-08 20:36:35 +0900

    fix: do not use `formatUnits` in viem

--- components/modal/withdraw-modal.tsx
+++ components/modal/withdraw-modal.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { formatUnits } from 'viem'
 
 import CurrencyAmountInput from '../input/currency-amount-input'
 import Modal from '../../components/modal/modal'
-import { BigDecimal } from '../../utils/numbers'
+import { BigDecimal, formatUnits } from '../../utils/numbers'
 import { ActionButton, ActionButtonProps } from '../button/action-button'
 import { Currency } from '../../model/currency'
 
@@ -50,12 +49,20 @@ const WithdrawModal = ({
       </div>
       <div className="flex text-xs sm:text-sm gap-3 mb-2 sm:mb-3 justify-between sm:justify-start">
         <span className="text-gray-500">Your deposit amount</span>
-        {formatUnits(depositAmount, depositCurrency.decimals)}{' '}
+        {formatUnits(
+          depositAmount,
+          depositCurrency.decimals,
+          depositAssetPrice,
+        )}{' '}
         {depositCurrency.symbol}
       </div>
       <div className="flex text-xs sm:text-sm gap-3 mb-6 sm:mb-8 justify-between sm:justify-start">
         <span className="text-gray-500">Coupon repurchase fee</span>
-        {formatUnits(repurchaseFee, depositCurrency.decimals)}{' '}
+        {formatUnits(
+          repurchaseFee,
+          depositCurrency.decimals,
+          depositAssetPrice,
+        )}{' '}
         {depositCurrency.symbol}
       </div>
       <ActionButton {...actionButtonProps} />
