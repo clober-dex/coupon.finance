1a9cd03e5142f860a4e308082cc6ba24f5f4646a
graykode <nlkey2022@gmail.com>
2023-11-09 20:56:38 +0900

    feat: flex-row header

---
+++ components/footer.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+
+import '../styles/globals.css'
+import { Footer } from './footer'
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  parameters: {
+    layout: 'centered',
+  },
+  render: () => (
+    <div className="w-screen">
+      <Footer />
+    </div>
+  ),
+} as Meta<typeof Footer>
+
+type Story = StoryObj<typeof Footer>
+export const Default: Story = {
+  args: {},
+}
