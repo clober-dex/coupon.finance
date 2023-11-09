a22d15ba8673c300c5e64033ea47f152521da6be
graykode <nlkey2022@gmail.com>
2023-11-09 15:22:08 +0900

    fix: conformation modal gap

--- components/modal/confirmation-modal.tsx
+++ components/modal/confirmation-modal.tsx
@@ -32,7 +32,7 @@ const ConfirmationModal = ({
             {confirmation.body}
           </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-1">
           {confirmation.fields.map((field, index) => (
             <div
               key={index}
