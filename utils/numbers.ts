b251fe49c2cbe5765e9adc8ef075c2ba7de3ab0f
Xavier <ladig1017@gmail.com>
2023-11-13 15:30:02 +0900

    fix: remove trailing zero

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -35,13 +35,14 @@ export const formatUnits = (
 ): string => {
   const formatted = _formatUnits(value, decimals)
   if (!price) {
-    return formatted
+    return formatted.replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1')
   }
   const priceValue = Number(price.value) / 10 ** price.decimals
   const underHalfPennyDecimals =
     Math.floor(Math.max(-Math.log10(0.005 / priceValue), 0) / 2) * 2
-  const fixed = new BigNumber(formatted)
-    .toFixed(underHalfPennyDecimals)
-    .replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/, '$1')
-  return +fixed === 0 ? formatted : fixed
+  const fixed = new BigNumber(formatted).toFixed(underHalfPennyDecimals)
+  return (+fixed === 0 ? formatted : fixed).replace(
+    /([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/,
+    '$1',
+  )
 }
