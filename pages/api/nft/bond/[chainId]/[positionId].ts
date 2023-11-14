34ff1719eeaa5bee77dc4e2fe9c6937756f3e663
graykode <nlkey2022@gmail.com>
2023-11-15 00:25:19 +0900

    refactor: move page

--- pages/api/nft/bond/[chainId]/[positionId].ts
+++ pages/api/nft/bond/[chainId]/[positionId].ts
@@ -1,68 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-import { fetchPrices } from '../../../../../apis/currency'
-import { formatUnits } from '../../../../../utils/numbers'
-import { fetchBondPosition } from '../../../../../apis/bond-position'
-import bondSvg from '../bond-svg'
-import { formatDate } from '../../../../../utils/date'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
-  try {
-    const query = req.query
-    const { positionId, chainId } = query
-    if (
-      !positionId ||
-      !chainId ||
-      typeof positionId !== 'string' ||
-      typeof chainId !== 'string'
-    ) {
-      res.json({
-        status: 'error',
-        message: 'URL should be /api/nft/bond/[positionId]?chainId=[chainId]',
-      })
-      return
-    }
-    const bondPosition = await fetchBondPosition(
-      Number(chainId),
-      BigInt(positionId),
-    )
-    if (!bondPosition) {
-      res.json({
-        status: 'error',
-        message: 'Something went wrong, while fetching bond position',
-      })
-      return
-    }
-    const prices = await fetchPrices(Number(chainId), [
-      bondPosition.underlying.address,
-    ])
-    const expiresAt = formatDate(
-      new Date(Number(bondPosition.toEpoch.endTimestamp) * 1000),
-    )
-    const svg = bondSvg
-      .replace(
-        /DEPOSIT_AMOUNT/g,
-        formatUnits(
-          bondPosition.amount,
-          bondPosition.underlying.decimals,
-          prices[bondPosition.underlying.address],
-        ),
-      )
-      .replace(/DEPOSIT_TOKEN/g, bondPosition.underlying.symbol)
-      .replace(/EXPIRES_AT/g, expiresAt)
-
-    res
-      .writeHead(200, {
-        'Content-Type': 'image/svg+xml',
-      })
-      .end(svg)
-  } catch (error) {
-    res.json({
-      status: 'error',
-      message: `Something went wrong, please try again!!!: ${error}`,
-    })
-  }
+  const query = req.query
+  const { positionId, chainId } = query
+  res.json({
+    id: positionId,
+    name: `Bond #${positionId}`,
+    description: 'Coupon Finance Bond',
+    image: `https://coupon.finance/api/nft/bond/${chainId}/images/${positionId}`,
+  })
 }
