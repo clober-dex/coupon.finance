5f49bd1b06f179e49d4332a89386b8f6ba52e256
graykode <nlkey2022@gmail.com>
2023-11-15 14:40:37 +0900

    feat: set subgraph urls

--- constants/subgraph-url.ts
+++ constants/subgraph-url.ts
@@ -3,7 +3,8 @@ import { CHAIN_IDS } from './chain'
 export const SUBGRAPH_URL: {
   [chain in CHAIN_IDS]: string
 } = {
-  [CHAIN_IDS.ARBITRUM]: '',
+  [CHAIN_IDS.ARBITRUM]:
+    'https://api.studio.thegraph.com/query/49804/coupon-subgraph/v1.0.1',
   [CHAIN_IDS.COUPON_FINANCE_CHAIN]:
     'https://dev-subgraph.coupon.finance/subgraphs/name/coupon-subgraph',
 }
@@ -11,7 +12,8 @@ export const SUBGRAPH_URL: {
 export const POINT_SUBGRAPH_URL: {
   [chain in CHAIN_IDS]: string
 } = {
-  [CHAIN_IDS.ARBITRUM]: '',
+  [CHAIN_IDS.ARBITRUM]:
+    'https://api.studio.thegraph.com/query/49804/coupon-point-subgraph/v1',
   [CHAIN_IDS.COUPON_FINANCE_CHAIN]:
     'https://dev-subgraph.coupon.finance/subgraphs/name/coupon-point-subgraph',
 }
