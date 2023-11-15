a5af6aefe6f57869c54a86cc4582f4c8881b4e86
graykode <nlkey2022@gmail.com>
2023-11-15 17:18:20 +0900

    fix: remove point subgraph temporary

--- apis/point.ts
+++ apis/point.ts
@@ -1,70 +1,73 @@
-import { getBuiltGraphSDK } from '../.graphclient'
-import { POINT_SUBGRAPH_URL } from '../constants/subgraph-url'
+// import { getBuiltGraphSDK } from '../.graphclient'
+// import { POINT_SUBGRAPH_URL } from '../constants/subgraph-url'
 import { CHAIN_IDS } from '../constants/chain'
 import { Point } from '../model/point'
 
-const { getBondPositionPoints, getLoanPositionPoints, getUserPoint } =
-  getBuiltGraphSDK()
+// const { getBondPositionPoints, getLoanPositionPoints, getUserPoint } =
+//   getBuiltGraphSDK()
 
 export async function fetchPoints(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   chainId: CHAIN_IDS,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   userAddress: `0x${string}`,
 ): Promise<Point[]> {
-  const [{ bondPositionPoints }, { loanPositionPoints }, { userPoint }] =
-    await Promise.all([
-      getBondPositionPoints(
-        {
-          userAddress: userAddress.toLowerCase(),
-        },
-        {
-          url: POINT_SUBGRAPH_URL[chainId],
-        },
-      ),
-      getLoanPositionPoints(
-        {
-          userAddress: userAddress.toLowerCase(),
-        },
-        {
-          url: POINT_SUBGRAPH_URL[chainId],
-        },
-      ),
-      getUserPoint(
-        {
-          user: userAddress.toLowerCase(),
-        },
-        {
-          url: POINT_SUBGRAPH_URL[chainId],
-        },
-      ),
-    ])
-  return [
-    ...bondPositionPoints.map((point) => ({
-      amount: BigInt(point.amount),
-      decimals: Number(point.decimals),
-      price: BigInt(point.price),
-      priceDecimals: Number(point.priceDecimals),
-      accumulatedPoint: BigInt(point.accumulatedPoint),
-      updatedAt: BigInt(point.updatedAt),
-    })),
-    ...loanPositionPoints.map((point) => ({
-      amount: BigInt(point.collateralAmount),
-      decimals: Number(point.collateralDecimals),
-      price: BigInt(point.collateralPrice),
-      priceDecimals: Number(point.collateralPriceDecimals),
-      accumulatedPoint: BigInt(point.accumulatedPoint),
-      updatedAt: BigInt(point.updatedAt),
-    })),
-    ...(userPoint
-      ? [
-          {
-            amount: 0n,
-            decimals: 0,
-            price: 0n,
-            priceDecimals: 0,
-            accumulatedPoint: BigInt(userPoint.point),
-            updatedAt: BigInt(userPoint.updatedAt),
-          },
-        ]
-      : []),
-  ]
+  // const [{ bondPositionPoints }, { loanPositionPoints }, { userPoint }] =
+  //   await Promise.all([
+  //     getBondPositionPoints(
+  //       {
+  //         userAddress: userAddress.toLowerCase(),
+  //       },
+  //       {
+  //         url: POINT_SUBGRAPH_URL[chainId],
+  //       },
+  //     ),
+  //     getLoanPositionPoints(
+  //       {
+  //         userAddress: userAddress.toLowerCase(),
+  //       },
+  //       {
+  //         url: POINT_SUBGRAPH_URL[chainId],
+  //       },
+  //     ),
+  //     getUserPoint(
+  //       {
+  //         user: userAddress.toLowerCase(),
+  //       },
+  //       {
+  //         url: POINT_SUBGRAPH_URL[chainId],
+  //       },
+  //     ),
+  //   ])
+  // return [
+  //   ...bondPositionPoints.map((point) => ({
+  //     amount: BigInt(point.amount),
+  //     decimals: Number(point.decimals),
+  //     price: BigInt(point.price),
+  //     priceDecimals: Number(point.priceDecimals),
+  //     accumulatedPoint: BigInt(point.accumulatedPoint),
+  //     updatedAt: BigInt(point.updatedAt),
+  //   })),
+  //   ...loanPositionPoints.map((point) => ({
+  //     amount: BigInt(point.collateralAmount),
+  //     decimals: Number(point.collateralDecimals),
+  //     price: BigInt(point.collateralPrice),
+  //     priceDecimals: Number(point.collateralPriceDecimals),
+  //     accumulatedPoint: BigInt(point.accumulatedPoint),
+  //     updatedAt: BigInt(point.updatedAt),
+  //   })),
+  //   ...(userPoint
+  //     ? [
+  //         {
+  //           amount: 0n,
+  //           decimals: 0,
+  //           price: 0n,
+  //           priceDecimals: 0,
+  //           accumulatedPoint: BigInt(userPoint.point),
+  //           updatedAt: BigInt(userPoint.updatedAt),
+  //         },
+  //       ]
+  //     : []),
+  // ]
+  return []
 }
