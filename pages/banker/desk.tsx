246fda5d69e822c5ab110a95a7705d4b518dffe1
graykode <nlkey2022@gmail.com>
2023-11-16 22:26:27 +0900

    feat: show animate-spin

--- pages/banker/desk.tsx
+++ pages/banker/desk.tsx
@@ -112,6 +112,16 @@ const CouponUtilsForm = ({
             </div>
           </div>
           <div className="flex justify-between flex-col relative bg-white dark:bg-gray-900 rounded-lg p-4 pb-8 sm:pb-0 sm:h-[90px]">
+            {!depositCurrency ? (
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div
+                  className="h-4 w-4 animate-spin rounded-full border-2 border-solid border-green-500 border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                  role="status"
+                />
+              </div>
+            ) : (
+              <></>
+            )}
             {dates.length > 0 ? (
               <div className="sm:px-6 sm:mb-2 mr-4 sm:mr-0">
                 <div>
