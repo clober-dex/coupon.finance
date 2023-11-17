1d7dd03c2a4533fd9763caf57fb4e216afd8dd6a
graykode <nlkey2022@gmail.com>
2023-11-17 15:06:12 +0900

    fix: hover -> group:hover

--- components/card/borrow-card.tsx
+++ components/card/borrow-card.tsx
@@ -28,8 +28,8 @@ export const BorrowCard = ({
   )
   return (
     <Link href={`/borrow/${currency.symbol}`}>
-      <div className="h-[300px] transition ease-in-out delay-150 duration-300 sm:hover:-translate-y-1 sm:hover:scale-105 group flex flex-col w-full px-4 py-6 justify-center items-center gap-4 bg-white dark:bg-gray-800 rounded-xl">
-        <div className="flex flex-col items-start gap-6 hover:gap-4 self-stretch">
+      <div className="transition ease-in-out delay-150 duration-300 sm:hover:-translate-y-1 sm:hover:scale-105 group flex flex-col w-full px-4 py-6 justify-center items-center gap-4 bg-white dark:bg-gray-800 rounded-xl">
+        <div className="flex flex-col items-start gap-6 group-hover:gap-4 self-stretch">
           <div className="flex items-center gap-3 self-stretch">
             <CurrencyIcon
               currency={currency}
