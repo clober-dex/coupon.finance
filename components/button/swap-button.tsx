36719ef4dcb0521cf2c28e9ba444dd334d51b23b
graykode <nlkey2022@gmail.com>
2023-11-20 21:20:09 +0900

    fix: swap button design

--- components/button/swap-button.tsx
+++ components/button/swap-button.tsx
@@ -9,11 +9,11 @@ export const SwapButton = ({
 }) => {
   return (
     <button
-      className="flex items-center justify-center gap-2 md:justify-start w-8 rounded sm:rounded-lg sm:w-full py-0 sm:px-3 cursor-pointer h-8 bg-gray-50 md:bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 active::bg-gray-600 text-base"
+      className="flex items-center justify-center gap-2 lg:justify-start w-8 h-8 rounded lg:rounded-lg lg:w-full py-0 lg:px-2.5 cursor-pointer bg-gray-50 md:bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 active::bg-gray-600 text-base"
       onClick={() => setShowSwapModal(true)}
     >
-      <ArrowLeftRightSvg className="w-full sm:w-4 sm:h-4" />
-      <span className="hidden md:block font-semibold text-sm">
+      <ArrowLeftRightSvg className="w-4 h-4" />
+      <span className="hidden xl:block font-semibold text-sm">
         Switch Tokens
       </span>
     </button>
