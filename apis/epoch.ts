1932c669c700733c0f692b278628632171bb5e3d
graykode <nlkey2022@gmail.com>
2023-11-22 18:58:39 +0900

    refactor: remove unused function

--- apis/epoch.ts
+++ apis/epoch.ts
@@ -1,28 +1,5 @@
 import { Epoch } from '../model/epoch'
-import { getBuiltGraphSDK, getIntegratedQuery } from '../.graphclient'
-import { SUBGRAPH_URL } from '../constants/subgraph-url'
-import { CHAIN_IDS } from '../constants/chain'
-import { currentTimestampInSeconds } from '../utils/date'
-
-const { getEpochs } = getBuiltGraphSDK()
-
-export async function fetchEpochs(chainId: CHAIN_IDS): Promise<Epoch[]> {
-  const { epoches } = await getEpochs(
-    {
-      timestamp: currentTimestampInSeconds().toString(),
-    },
-    {
-      url: SUBGRAPH_URL[chainId],
-    },
-  )
-  return epoches
-    .map((epoch) => ({
-      id: +epoch.id,
-      startTimestamp: +epoch.startTimestamp,
-      endTimestamp: +epoch.endTimestamp,
-    }))
-    .sort((a, b) => a.endTimestamp - b.endTimestamp)
-}
+import { getIntegratedQuery } from '../.graphclient'
 
 export function extractEpochs(integrated: getIntegratedQuery | null): Epoch[] {
   if (!integrated) {
