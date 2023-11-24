c1007e4c13e9104aa2719ace8a7f59ccbcfe0e9a
graykode <nlkey2022@gmail.com>
2023-11-24 16:09:18 +0900

    feat: disable button when selling coupon is not profitable

--- components/modal/coupon-user-balance-modal.tsx
+++ components/modal/coupon-user-balance-modal.tsx
@@ -37,14 +37,12 @@ export const CouponUserBalanceModal = ({
   sellCoupons: (marketSellParams: CouponBalance[]) => Promise<void>
 }) => {
   const [clicked, setClicked] = useState(false)
-  if (
-    couponBalances.reduce((acc, { assetValue }) => acc + assetValue, 0n) === 0n
-  ) {
+  if (couponBalances.reduce((acc, { balance }) => acc + balance, 0n) === 0n) {
     return <></>
   }
 
   const sellAvailableCoupons = couponBalances.filter(
-    ({ assetValue }) => assetValue > 0n,
+    ({ balance }) => balance > 0n,
   )
 
   return (
@@ -58,14 +56,20 @@ export const CouponUserBalanceModal = ({
               <div className="absolute -right-2 sm:right-0 bottom-3/4 mx-4 my-8 bg-white dark:bg-gray-800 rounded-lg">
                 <div className="flex flex-col items-start w-max">
                   <div className="flex px-4 pt-3 pb-1 items-center self-stretch text-sm font-semibold">
-                    <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 flex-grow shrink-0 basis-0 text-sm">
+                    <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 flex-grow shrink-0 basis-0 text-sm dark:text-white text-gray-950">
                       My Coupons
                     </div>
                     <button
                       onClick={async () => {
                         await sellCoupons(sellAvailableCoupons)
                       }}
-                      className="flex flex-col my-1 w-16 h-7 sm:h-8 justify-center items-center rounded bg-green-500 bg-opacity-10 text-xs text-opacity-90 font-semibold text-green-500"
+                      disabled={
+                        sellAvailableCoupons.reduce(
+                          (acc, { assetValue }) => acc + assetValue,
+                          0n,
+                        ) === 0n
+                      }
+                      className="flex flex-col my-1 w-16 h-7 sm:h-8 justify-center items-center rounded bg-opacity-10 text-xs text-opacity-90 font-semibold text-green-500 bg-green-500 hover:bg-green-300 hover:bg-opacity-10 dark:hover:bg-gray-600 disabled:bg-gray-100 dark:disabled:bg-gray-800 disabled:text-gray-300 dark:disabled:text-gray-500"
                     >
                       Sell All
                     </button>
@@ -105,11 +109,12 @@ export const CouponUserBalanceModal = ({
                             </div>
                           </div>
                           <button
+                            disabled={assetValue === 0n}
                             onClick={async () => {
                               await sellCoupons([sellAvailableCoupons[index]])
                               setClicked(false)
                             }}
-                            className="flex flex-col my-1 w-16 h-7 sm:h-8 justify-center items-center rounded bg-green-500 bg-opacity-10 text-xs text-opacity-90 font-semibold text-green-500"
+                            className="flex flex-col my-1 w-16 h-7 sm:h-8 justify-center items-center rounded bg-opacity-10 text-xs text-opacity-90 font-semibold text-green-500 bg-green-500 hover:bg-green-300 hover:bg-opacity-10 dark:hover:bg-gray-600 disabled:bg-gray-100 dark:disabled:bg-gray-800 disabled:text-gray-300 dark:disabled:text-gray-500"
                           >
                             Sell
                           </button>
