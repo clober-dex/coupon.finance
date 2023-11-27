278968d16dccd8fb130dc930519121baf264a5d1
graykode <nlkey2022@gmail.com>
2023-11-27 11:36:03 +0900

    feat: new design

--- components/modal/coupon-user-balance-modal.tsx
+++ components/modal/coupon-user-balance-modal.tsx
@@ -55,7 +55,7 @@ export const CouponUserBalanceModal = ({
             <CouponWidget setClicked={setClicked}>
               <div className="absolute -right-2 sm:right-0 bottom-3/4 mx-4 my-8 bg-white dark:bg-gray-800 rounded-lg">
                 <div className="flex flex-col items-start w-max">
-                  <div className="flex px-4 pt-3 pb-1 items-center self-stretch text-sm font-semibold">
+                  <div className="flex px-4 pt-3 pb-2 items-center self-stretch text-sm font-semibold">
                     <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 flex-grow shrink-0 basis-0 text-sm dark:text-white text-gray-950">
                       My Coupons
                     </div>
@@ -84,28 +84,42 @@ export const CouponUserBalanceModal = ({
                             : 'pb-1'
                         } items-center self-stretch`}
                       >
-                        <div className="flex items-center flex-grow shrink-0 gap-3 basis-0">
-                          <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 flex-grow shrink-0 basis-0 text-sm">
-                            <div className="text-black dark:text-white">
-                              {toPlacesString(
-                                formatUnits(balance, market.baseToken.decimals),
-                              )}{' '}
-                              {market.baseToken.symbol}
-                            </div>
-                            <div className="text-gray-500 dark:text-gray-300">
-                              (
-                              {formatDate(
-                                new Date(Number(market.endTimestamp) * 1000),
-                              )}
-                              )
-                            </div>
-                            <div className="text-gray-500 dark:text-gray-300">
-                              {toPlacesString(
-                                formatUnits(
-                                  assetValue,
-                                  market.quoteToken.decimals,
-                                ),
-                              )}
+                        <div className="flex items-center flex-grow shrink-0 gap-4 basis-0">
+                          <div className="flex p-2 items-center gap-1 flex-grow shrink-0 basis-0 bg-gray-50 dark:bg-gray-700 rounded-lg">
+                            <div className="flex flex-col justify-center items-start gap-3 flex-grow shrink-0 basis-0">
+                              <div className="text-sm">
+                                {toPlacesString(
+                                  formatUnits(
+                                    balance,
+                                    market.baseToken.decimals,
+                                  ),
+                                )}{' '}
+                                {market.baseToken.symbol}
+                              </div>
+                              <div className="flex flex-col justify-center items-start gap-2">
+                                <div className="flex items-center gap-3 text-xs w-[180px] sm:w-[228px]">
+                                  <span className="text-gray-400">Expires</span>
+                                  <span className="text-gray-500 dark:text-gray-300">
+                                    {formatDate(
+                                      new Date(
+                                        Number(market.endTimestamp) * 1000,
+                                      ),
+                                    )}
+                                  </span>
+                                </div>
+                                <div className="flex items-center gap-2 text-xs">
+                                  <span className="text-gray-400">Value</span>
+                                  <span className="text-gray-500 dark:text-gray-300">
+                                    {toPlacesString(
+                                      formatUnits(
+                                        assetValue,
+                                        market.quoteToken.decimals,
+                                      ),
+                                    )}{' '}
+                                    {market.quoteToken.symbol}
+                                  </span>
+                                </div>
+                              </div>
                             </div>
                           </div>
                           <button
