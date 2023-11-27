448b80995a9ef2a02b0fa956d3000ff73d0259d4
graykode <nlkey2022@gmail.com>
2023-11-27 19:55:05 +0900

    feat: use `assetValue` as only asset value for erc1155

--- model/coupon-balance.ts
+++ model/coupon-balance.ts
@@ -3,7 +3,7 @@ import { Market } from './market'
 export type CouponBalance = {
   market: Market
   balance: bigint // erc20 + erc1155 balance
-  assetValue: bigint
+  assetValue: bigint // erc1155 balance * price
   erc20Balance: bigint
   erc1155Balance: bigint
 }
