5695ea0256cf29a039cfd369828049d104c40864
graykode <nlkey2022@gmail.com>
2023-12-01 00:09:25 +0900

    feat: add adjust leverage model dumb component

---
+++ components/modal/adjust-leverage-modal.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import '../../styles/globals.css'
+
+import { dummyLoanPosition } from '../../.storybook/dummy-data/loan-position'
+
+import AdjustLeverageModal from './adjust-leverage-modal'
+
+export default {
+  title: 'Modal/AdjustLeverageModal',
+  component: AdjustLeverageModal,
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta<typeof AdjustLeverageModal>
+
+type Story = StoryObj<typeof AdjustLeverageModal>
+
+export const Default: Story = {
+  args: {
+    onClose: () => {},
+    multiple: 3,
+    collateral: dummyLoanPosition.collateral,
+    setMultiple: () => {},
+    maxAvailableMultiple: 5,
+    currentMultiple: 2,
+    currentLtv: 60,
+    expectedLtv: 70,
+    actionButtonProps: {
+      disabled: false,
+      onClick: () => {},
+      text: 'Edit Multiple',
+    },
+  },
+}
+
+// @ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString()
+}
---
+++ components/modal/adjust-leverage-modal.tsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+import Modal from '../../components/modal/modal'
+import { ActionButton, ActionButtonProps } from '../button/action-button'
+import Slider from '../slider'
+import { DotSvg } from '../svg/dot-svg'
+import { getLTVTextColor } from '../../utils/ltv'
+import { ArrowSvg } from '../svg/arrow-svg'
+import { Collateral } from '../../model/collateral'
+
+const AdjustLeverageModal = ({
+  onClose,
+  collateral,
+  multiple,
+  setMultiple,
+  maxAvailableMultiple,
+  currentMultiple,
+  currentLtv,
+  expectedLtv,
+  actionButtonProps,
+}: {
+  onClose: () => void
+  collateral: Collateral
+  multiple: number
+  setMultiple: (value: number) => void
+  maxAvailableMultiple: number
+  currentMultiple: number
+  currentLtv: number
+  expectedLtv: number
+  actionButtonProps: ActionButtonProps
+}) => {
+  return (
+    <Modal show onClose={onClose}>
+      <h1 className="flex font-bold text-xl mb-3">
+        Set multiple.
+        <span className="ml-auto font-semibold text-gray-400 text-sm">
+          Max {maxAvailableMultiple.toFixed(2)}x
+        </span>
+      </h1>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col bg-gray-50 dark:bg-gray-900">
+          <div className="flex justify-between flex-col relative rounded-lg pl-4 pr-12 sm:pl-0 sm:pr-6 sm:py-10">
+            <div className="sm:px-6 sm:mb-2 my-8 sm:my-0">
+              <div>
+                <Slider
+                  minPosition={0}
+                  segments={(maxAvailableMultiple - 1) * 100}
+                  value={(multiple - 1) * 100}
+                  onValueChange={
+                    (value) => setMultiple(value / 100 + 1) // value is 0-based
+                  }
+                  renderControl={() => (
+                    <div className="absolute -top-3 -left-7 flex flex-col items-center gap-2 shrink-0">
+                      <DotSvg />
+                      <div className="flex px-2 py-1 justify-center w-[60px] items-center gap-1 rounded-2xl bg-green-500 bg-opacity-10 text-xs text-green-500 font-bold">
+                        {multiple.toFixed(2)} x
+                      </div>
+                    </div>
+                  )}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-start self-stretch">
+              <div className="text-gray-400 text-base">LTV</div>
+              <div className="flex ml-auto items-center gap-1.5 text-base text-black dark:text-white">
+                <span className={`${getLTVTextColor(currentLtv, collateral)}`}>
+                  {currentLtv.toFixed(2)}%
+                </span>
+                {currentMultiple !== multiple ? (
+                  <>
+                    <ArrowSvg />
+                    <span
+                      className={`${getLTVTextColor(expectedLtv, collateral)}`}
+                    >
+                      {expectedLtv.toFixed(2)}%
+                    </span>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+            <div className="flex items-start self-stretch">
+              <div className="text-gray-400 text-base">Multiple</div>
+              <div className="flex ml-auto items-center gap-1.5 text-base text-black dark:text-white">
+                {currentMultiple.toFixed(2)}x
+                {currentMultiple !== multiple ? (
+                  <>
+                    <ArrowSvg />
+                    {multiple.toFixed(2)}x
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <ActionButton {...actionButtonProps} />
+      </div>
+    </Modal>
+  )
+}
+
+export default AdjustLeverageModal
