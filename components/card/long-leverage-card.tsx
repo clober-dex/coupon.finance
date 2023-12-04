fd1389673882014700d1c47e2b38d3c4c311af55
graykode <nlkey2022@gmail.com>
2023-12-05 00:38:47 +0900

    fix: debtCurrencies

--- components/card/long-leverage-card.tsx
+++ components/card/long-leverage-card.tsx
@@ -21,7 +21,13 @@ export const LongLeverageCard = ({
   prices: Prices
 }) => {
   return (
-    <Link href={`/leverage/${collateralCurrency.symbol}_LONG`}>
+    <Link
+      href={
+        debtCurrencies.length === 1
+          ? `/leverage/${collateralCurrency.symbol}_${debtCurrencies[0].symbol}`
+          : `/leverage/${collateralCurrency.symbol}_LONG`
+      }
+    >
       <div className="h-[314px] flex flex-col w-full px-4 py-6 justify-center items-center gap-4 bg-white dark:bg-gray-800 rounded-xl">
         <div className="flex flex-col items-start gap-6 self-stretch">
           <div className="flex items-center gap-3 self-stretch">
