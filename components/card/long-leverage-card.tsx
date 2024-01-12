61ec4b18c9064a635f93a627b7cbb1af29075fb1
graykode <nlkey2022@gmail.com>
2024-01-12 16:49:02 +0900

    fix: md:absolute

--- components/card/long-leverage-card.tsx
+++ components/card/long-leverage-card.tsx
@@ -157,7 +157,7 @@ export const LongLeverageCard = ({
                 </div>
               ))}
             </div>
-            <div className="w-full flex sm:absolute sm:bottom-4 sm:h-12 md:w-[264px] lg:w-[277px] flex-col items-center justify-center self-stretch bg-green-500 rounded-lg h-12 px-3 py-2 font-bold text-base text-white gap-2 hover:bg-green-400 dark:hover:bg-green-600">
+            <div className="w-full flex md:absolute sm:bottom-4 sm:h-12 md:w-[264px] lg:w-[277px] flex-col items-center justify-center self-stretch bg-green-500 rounded-lg h-12 px-3 py-2 font-bold text-base text-white gap-2 hover:bg-green-400 dark:hover:bg-green-600">
               Leverage
             </div>
           </div>
