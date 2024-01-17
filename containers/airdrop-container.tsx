71c6a95e561e5f0e5bb528fca84facc75ff011ad
graykode <nlkey2022@gmail.com>
2024-01-17 14:16:12 +0900

    feat: missing component

--- containers/airdrop-container.tsx
+++ containers/airdrop-container.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAccount } from 'wagmi'
+import Link from 'next/link'
 
 import { NumberOneBoxSvg } from '../components/svg/number-one-box-svg'
 import { NumberTwoBoxSvg } from '../components/svg/number-two-box-svg'
@@ -24,6 +26,8 @@ import { CouponMobileIconSvg } from '../components/svg/coupon-mobile-icon-svg'
 import { CouponPCIconSvg } from '../components/svg/coupon-pc-icon-svg'
 import { MoneyTopIconSvg } from '../components/svg/money-top-icon-svg'
 import { MoneyBottomIconSvg } from '../components/svg/money-bottom-icon-svg'
+import { usePointContext } from '../contexts/point-context'
+import { UpBracketAngleSvg } from '../components/svg/up-bracket-angle-svg'
 
 const LeaderboardTab = () => {
   return (
@@ -127,10 +131,17 @@ const LeaderboardTab = () => {
   )
 }
 
-const ReferralTab = () => {
+const ReferralTab = ({
+  referralCode,
+  referentCode,
+}: {
+  referralCode: string
+  referentCode: string | null
+}) => {
   const [mode, setMode] = React.useState<'setReferralCode' | 'getReferralCode'>(
     'setReferralCode',
   )
+  const [value, setValue] = React.useState<string>(referentCode || '')
 
   return (
     <div className="flex mx-4 p-4 lg:pt-8 lg:pb-8 flex-col items-start gap-8 lg:gap-16 self-stretch bg-white dark:bg-gray-850 rounded-2xl mt-8">
@@ -158,7 +169,7 @@ const ReferralTab = () => {
             <div className="flex flex-col gap-2 lg:gap-4">
               <div className="text-base lg:text-xl font-semibold text-center lg:text-start">
                 Accepting a referral <br />
-                boosts your points by 10%!
+                boosts your points by up to 10%!
               </div>
               <div className="text-gray-500 text-xs lg:text-base text-center lg:text-start">
                 Please enter your fren’s code below{' '}
@@ -170,6 +181,8 @@ const ReferralTab = () => {
             <input
               className="w-full outline-none lg:w-[386px] bg-gray-50 dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-600 text-start pl-4 text-sm h-12"
               placeholder="Please input the referral code"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
             />
             <button className="w-full lg:w-[160px] font-bold bg-green-500 hover:bg-green-400 dark:hover:bg-green-600 disabled:bg-gray-100 dark:disabled:bg-gray-800 h-12 rounded-lg text-white disabled:text-gray-300 dark:disabled:text-gray-500">
               Accept
@@ -195,10 +208,18 @@ const ReferralTab = () => {
             <div className="flex flex-col lg:flex-row justify-center items-center gap-1">
               <div className="text-sm lg:text-base">Your referral code is</div>
               <div className="flex gap-1 text-sm lg:text-base text-green-500 font-semibold">
-                0xf7xfdh1231kdflm2f
-                <div className="flex items-center">
+                {referralCode}
+                <button
+                  className="flex items-center"
+                  onClick={
+                    () =>
+                      navigator.clipboard.writeText(
+                        `http://localhost:3000/?mode=airdrop&referralCode=${referralCode}`,
+                      ) // todo: change to real url
+                  }
+                >
                   <CopyIconSvg />
-                </div>
+                </button>
               </div>
             </div>
           </div>
@@ -263,9 +284,19 @@ const ClaimTab = () => {
 }
 
 export const AirdropContainer = () => {
+  const { referralCode, referentCode, hasReferent } = usePointContext()
+  const { address: userAddress } = useAccount()
+
   const [mode, setMode] = React.useState<'leaderboard' | 'referral' | 'claim'>(
     'leaderboard',
   )
+  const [show, setShow] = React.useState<boolean>(false)
+
+  useEffect(() => {
+    if (!hasReferent && referentCode && userAddress) {
+      setMode('referral')
+    }
+  }, [hasReferent, referentCode, userAddress])
 
   return (
     <div className="flex flex-col items-center">
@@ -345,20 +376,97 @@ export const AirdropContainer = () => {
             <NumberThreeBoxSvg className="h-4 w-4" /> Claim $CPN
           </div>
         </div>
-        <div className="flex lg:hidden justify-center">
+
+        <div className="flex flex-col lg:hidden justify-center">
           <div className="flex px-4 py-3 justify-center w-full text-sm lg:text-xl font-semibold">
             <div>How can I get points?</div>
-            <DownBracketAngleSvg className="ml-44" />
+            <button onClick={() => setShow(!show)}>
+              {show ? (
+                <UpBracketAngleSvg className="ml-44" />
+              ) : (
+                <DownBracketAngleSvg className="ml-44" />
+              )}
+            </button>
           </div>
+          {show ? (
+            <div className="flex flex-col items-center gap-4 self-stretch">
+              <div className="flex flex-col items-center gap-2">
+                <div className="flex w-[328px] p-4 flex-col items-center gap-4 rounded-xl ring-2 ring-gray-200">
+                  <div className="flex flex-col items-start gap-3 self-stretch">
+                    <div className="flex flex-col items-start gap-2">
+                      <div className="flex flex-row gap-2 text-sm font-semibold items-center">
+                        <DepositIconSvg className="w-4 h-4" />
+                        Deposit points
+                      </div>
+                      <div className="text-xs font-semibold">
+                        For every $1 in trades, earn 1 point per hour.
+                      </div>
+                    </div>
+                  </div>
+                  <button className="flex w-full h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
+                    Go to deposit
+                  </button>
+                </div>
+                <div className="flex w-[328px] p-4 flex-col items-center gap-4 rounded-xl ring-2 ring-gray-200">
+                  <div className="flex flex-col items-start gap-3 self-stretch">
+                    <div className="flex flex-col items-start gap-2">
+                      <div className="flex flex-row gap-2 text-sm font-semibold items-center">
+                        <BorrowIconSvg className="w-4 h-4" />
+                        Borrow points
+                      </div>
+                      <div className="text-xs font-semibold">
+                        For every $1 in trades, earn 1 point per hour.
+                      </div>
+                    </div>
+                  </div>
+                  <button className="flex w-full h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
+                    Go to borrow
+                  </button>
+                </div>
+                <div className="flex w-[328px] p-4 flex-col items-center gap-4 rounded-xl ring-2 ring-gray-200">
+                  <div className="flex flex-col items-start gap-3 self-stretch">
+                    <div className="flex flex-col items-start gap-2">
+                      <div className="flex flex-row gap-2 text-sm font-semibold items-center">
+                        <ReferralIconSvg className="stroke-gray-950 dark:stroke-white stroke-2 w-4 h-4" />
+                        Referral points
+                      </div>
+                      <div className="text-xs font-semibold">
+                        You can get more from your referees’ points.
+                      </div>
+                    </div>
+                  </div>
+                  <button className="flex w-full h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
+                    Go to refer
+                  </button>
+                </div>
+              </div>
+              <div className="flex gap-1 items-center mb-4">
+                <Link
+                  href="https://docs.coupon.finance/tokenomics/points"
+                  target="_blank"
+                  className="h-6 text-green-500 font-semibold"
+                >
+                  See Point rule detail
+                </Link>
+                <RightBracketAngleSvg className="w-4 h-4 stroke-green-500" />
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
 
         <div className="hidden lg:flex flex-col gap-6 items-center">
           <div className="flex flex-row gap-4 w-[960px] items-end">
             <div className="text-2xl font-semibold">How can I get points?</div>
             <div className="flex gap-1 items-center">
-              <div className="h-6 text-green-500 font-semibold">
+              <Link
+                href="https://docs.coupon.finance/tokenomics/points"
+                target="_blank"
+                className="h-6 text-green-500 font-semibold"
+              >
                 See Point rule detail
-              </div>
+              </Link>
               <RightBracketAngleSvg className="w-4 h-4 stroke-green-500" />
             </div>
           </div>
@@ -369,8 +477,8 @@ export const AirdropContainer = () => {
                 Deposit points
               </div>
               <p className="font-semibold text-sm">
-                <span className="block">For every $1000 in trades,</span>
-                <span className="block">earn 40 points.</span>
+                <span className="block">For every $1 in trades,</span>
+                <span className="block">earn 1 point per hour.</span>
               </p>
               <button className="flex w-[267px] h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
                 Go to deposit
@@ -382,8 +490,8 @@ export const AirdropContainer = () => {
                 Borrow points
               </div>
               <p className="font-semibold text-sm">
-                <span className="block">For every $1000 in trades,</span>
-                <span className="block">earn 40 points.</span>
+                <span className="block">For every $1 in trades,</span>
+                <span className="block">earn 1 point per hour.</span>
               </p>
               <button className="flex w-[267px] h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
                 Go to borrow
@@ -395,11 +503,11 @@ export const AirdropContainer = () => {
                 Referral points
               </div>
               <p className="font-semibold text-sm">
-                <span className="block">You can get 10% from</span>
+                <span className="block">You can get more from</span>
                 <span className="block">your referees’ points.</span>
               </p>
               <button className="flex w-[267px] h-8 bg-green-500 bg-opacity-10 rounded flex-col justify-center items-center opacity-90 text-center text-green-500 text-sm font-semibold">
-                Go to deposit
+                Go to refer
               </button>
             </div>
           </div>
@@ -417,7 +525,7 @@ export const AirdropContainer = () => {
           </button>
           <button
             disabled={mode === 'referral'}
-            onClick={() => setMode('referral')}
+            onClick={() => (userAddress ? setMode('referral') : null)}
             className="group w-[108px] lg:w-[220px] disabled:text-green-500 flex flex-row gap-1 lg:gap-2 items-center justify-center font-bold pb-1 lg:pb-3 border-b-2 border-solid disabled:border-b-green-500 text-gray-400 border-b-transparent"
           >
             <div className="relative">
@@ -427,7 +535,7 @@ export const AirdropContainer = () => {
           </button>
           <button
             disabled={mode === 'claim'}
-            onClick={() => setMode('claim')}
+            onClick={() => (userAddress ? setMode('claim') : null)}
             className="group w-[96px] lg:w-[220px] disabled:text-green-500 flex flex-row gap-1 lg:gap-2 items-center justify-center font-bold pb-1 lg:pb-3 border-b-2 border-solid disabled:border-b-green-500 text-gray-400 border-b-transparent"
           >
             <div className="relative">
@@ -440,8 +548,11 @@ export const AirdropContainer = () => {
       <div className="w-[360px] lg:w-[960px]">
         {mode === 'leaderboard' ? (
           <LeaderboardTab />
-        ) : mode === 'referral' ? (
-          <ReferralTab />
+        ) : mode === 'referral' && referralCode ? (
+          <ReferralTab
+            referralCode={referralCode}
+            referentCode={referentCode}
+          />
         ) : mode === 'claim' ? (
           <ClaimTab />
         ) : (
